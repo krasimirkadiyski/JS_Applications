e20a928a941fcce74432288629ff3eb89f362f59
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-07-04 12:39:02 +0300

    exercises

--- Lab-Asynchronous-Programming/02.Github-Repos/scripts/app.js
+++ Lab-Asynchronous-Programming/02.Github-Repos/scripts/app.js
@@ -5,21 +5,29 @@ function loadRepos() {
 	let url = `https://api.github.com/users/${username.value}/repos`;
 	let resultArr = [];
 	fetch(url)
-	.then(response => response.json())
-	.then(data => {
-		resultArr = data.map(e => e.full_name + "|" + e.owner.html_url);
-		resultArr.forEach(element => {
-			let [name,url] = element.split('|');
-			let li = document.createElement('li');
-			let a = document.createElement('a');
-			a.textContent = name;
-			a.href = url;
-			li.appendChild(a);
-			ul.appendChild(li);
-		});
-	})
-	.catch(err => {
-		console.log(err);
-	})
-	
+		.then(handleResponse)
+		.then(data => {
+			resultArr = data.map(e => e.full_name + "|" + e.owner.html_url);
+			resultArr.forEach(element => {
+				let [name, url] = element.split('|');
+				let li = document.createElement('li');
+				let a = document.createElement('a');
+				a.textContent = name;
+				a.href = url;
+				li.appendChild(a);
+				ul.appendChild(li);
+			});
+		})
+		.catch(handleError)
+	function handleResponse(response) {
+		if (!response.ok) {
+			throw new Error(`ERROR: ${response.status}!!!`)
+		}
+		return response.json();
+
+	}
+	function handleError(error){
+		ul.textContent = error.message;
+	}
+
 };
--- Lab-Asynchronous-Programming/03.Gighub-Commits/app.js
+++ Lab-Asynchronous-Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,23 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let ul = document.getElementById('commits');
+    ul.innerHTML = '';
+    try{
+    let promise  = fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+    let response = await promise;
+    if(!response.ok){
+        throw new Error(`ERROR: ${response.status}!`)
+    }
+    let data = await response.json();
+    console.log(data);
+    for (const current of data) {
+        ul.innerHTML += `<li>${current.commit.author.name}: ${current.commit.message}</li>`;
+    }
+    
+    }catch(error){
+        ul.innerHTML += `${error.message}!!!</li>`;
+    }
+
+    
 }
