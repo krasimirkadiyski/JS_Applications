8e66529fe80c5c86942b983ec48bc16e04d637c0
krasimirkadiyski <94919308+krasimirkadiyski@users.noreply.github.com>
2022-07-10 14:29:01 +0300

    exercises

--- Lab-Asynchronous-Programming/4.Cookbook-Part-01/app.js
+++ Lab-Asynchronous-Programming/4.Cookbook-Part-01/app.js
@@ -1,82 +1,72 @@
 async function getRecipes() {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
-    const recipes = await response.json();
-
-    return Object.values(recipes);
-}
-
-async function getRecipeById(id) {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/details/' + id);
-    const recipe = await response.json();
-
-    return recipe;
-}
-
-function createRecipePreview(recipe) {
-    const result = e('article', { className: 'preview', onClick: toggleCard },
-        e('div', { className: 'title' }, e('h2', {}, recipe.name)),
-        e('div', { className: 'small' }, e('img', { src: recipe.img })),
-    );
-
-    return result;
-
-    async function toggleCard() {
-        const fullRecipe = await getRecipeById(recipe._id);
-
-        result.replaceWith(createRecipeCard(fullRecipe));
+    try {
+        let response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
+        let data = await response.json();
+        let dataRecipies = Object.values(data)
+        return dataRecipies
     }
+    catch (error) {
+        console.log(error.message);
+    }
+    
 }
-
-function createRecipeCard(recipe) {
-    const result = e('article', {},
-        e('h2', {}, recipe.name),
-        e('div', { className: 'band' },
-            e('div', { className: 'thumb' }, e('img', { src: recipe.img })),
-            e('div', { className: 'ingredients' },
-                e('h3', {}, 'Ingredients:'),
-                e('ul', {}, recipe.ingredients.map(i => e('li', {}, i))),
-            )
-        ),
-        e('div', { className: 'description' },
-            e('h3', {}, 'Preparation:'),
-            recipe.steps.map(s => e('p', {}, s))
-        ),
-    );
-
-    return result;
+function createPreview(main,title,imgsrc,_id){
+   let article = e('article','','preview');
+   let div1 = e('div','','title',article);
+   let h2 = e('h2',title,'',div1);
+   let div2 = e('div', '', 'small', article);
+   let img = e('img', '', '', div2, imgsrc)
+   article.setAttribute('id', _id)
+   article.addEventListener('click', recipeInfo);
+   main.appendChild(article);
 }
+function createFullView(title,imgsrc,ingredients,steps,_id){
+    let article = e('article');
+    let h2 = e('h2',title,'',article);
+    let div1 = e('div','','band',article);
 
+    let div2 = e('div','','thumb',div1);
+
+    let img = e('img','','',div2,imgsrc);
+    let div3 = e('div','','ingredients',div1);
+    let h3 = e('h3','Ingredients:','',div3);
+    let ul = e('ul','','',div3);
+    ingredients.forEach(el =>{
+        e('li',el,'',ul);
+    });
+    let divDescription = e('div', '', 'description', article);
+    let h33 = e('h3','Preparation:','',divDescription);
+    steps.forEach(el => {
+        e('p',el,'',divDescription);
+    });
+    article.setAttribute('id',_id);
+    return article;
+    
+};
+function e(type, content, clazz,parent, src){
+    let el = document.createElement(type);
+    if(content){el.textContent = content;}
+    if(clazz){el.className = clazz;}
+    if(src){ el.src = src; }
+    if(parent){parent.appendChild(el); }
+    return el;
+    
+}
+async function recipeInfo(e){
+    let target = e.currentTarget;
+    let _id = target.id;
+    let response = await fetch(`http://localhost:3030/jsonstore/cookbook/details/${_id}`);
+    let data = await response.json();
+    console.log(data);
+    target.replaceWith(createFullView(data.name,data.img,data.ingredients,data.steps));
+
+}
 window.addEventListener('load', async () => {
     const main = document.querySelector('main');
-
-    const recipes = await getRecipes();
-    const cards = recipes.map(createRecipePreview);
-
     main.innerHTML = '';
-    cards.forEach(c => main.appendChild(c));
+    let dataRecipies = await getRecipes();
+    dataRecipies.forEach(e => {
+        createPreview(main,e.name,e.img,e._id)
+    })
+    
 });
-
-function e(type, attributes, ...content) {
-    const result = document.createElement(type);
-
-    for (let [attr, value] of Object.entries(attributes || {})) {
-        if (attr.substring(0, 2) == 'on') {
-            result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
-        } else {
-            result[attr] = value;
-        }
-    }
-
-    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
-
-    content.forEach(e => {
-        if (typeof e == 'string' || typeof e == 'number') {
-            const node = document.createTextNode(e);
-            result.appendChild(node);
-        } else {
-            result.appendChild(e);
-        }
-    });
-
-    return result;
-}
